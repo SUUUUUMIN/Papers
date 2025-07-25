acb942e9187c8ae41bd62be1f4860a993ea027d1
SUUUUUMIN <datalover_sumin@naver.com>
2025-07-25 13:16:20 +0900

    전송 방식을 텔레그램으로 변경

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Crawling import crawling_data
 from Summary import summarize_with_llm
 from Sending import send_automated_email
+from Telegram import send_telegram_msg
 
 papers, date = crawling_data()
 body = summarize_with_llm(papers)
-send_automated_email(date, body)
+send_telegram_msg(date, body) #Telegram
+#send_automated_email(date, body) #Email
