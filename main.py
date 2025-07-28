5dd717739886d915603613f05244c4cef6e0fac9
SUUUUUMIN <datalover_sumin@naver.com>
2025-07-28 14:35:47 +0900

    텔레그램 메시지 청킹 추가

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from Sending import send_automated_email
 from Telegram import send_telegram_msg
 
 papers, date = crawling_data()
-body = summarize_with_llm(papers)
-send_telegram_msg(date, body) #Telegram
+summarize = summarize_with_llm(papers)
+send_telegram_msg(date, summarize) #Telegram
 #send_automated_email(date, body) #Email
