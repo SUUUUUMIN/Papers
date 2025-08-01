18ff7ff484dfdc78701de6a244fa5e651887124c
SUUUUUMIN <datalover_sumin@naver.com>
2025-08-01 09:06:19 +0900

    이메일 코드 삭제

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from crawling import crawling_data
 from summary import summarize_with_llm
 from telegram import send_telegram_msg
 from notion import save_to_notion
-# from Sending import send_automated_email
 
 papers, date = crawling_data()
 summarize = summarize_with_llm(papers)
 send_telegram_msg(date, summarize) #Telegram
 save_to_notion(date, summarize) # Notion
-#send_automated_email(date, body) #Email
