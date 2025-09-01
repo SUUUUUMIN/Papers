b64ceb47742a781b79d1ca6fd1bf85583e25ddbd
SUUUUUMIN <datalover_sumin@naver.com>
2025-09-01 13:41:19 +0900

    텔레그램 중단

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from crawling import crawling_data
 from summary import summarize_with_llm
 from telegram import send_telegram_msg
 from notion import save_to_notion
+from send_email import send_automated_email
 
 papers, date = crawling_data()
 summarize = summarize_with_llm(papers)
-send_telegram_msg(date, summarize) #Telegram
+# send_telegram_msg(date, summarize) #Telegram
+send_automated_email(date, summarize)
 save_to_notion(date, summarize) # Notion
